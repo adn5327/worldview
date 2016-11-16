4ca79c52d6e387ebf04855dc5df53e3febbc6779
kkHz <hegemonolith@gmail.com>
2016-11-16 00:45:55 -0600

    Styling topic/source selection screens.

--- app/src/main/java/edu/illinois/g31/worldview/Sources.java
+++ app/src/main/java/edu/illinois/g31/worldview/Sources.java
@@ -3,6 +3,7 @@ package edu.illinois.g31.worldview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -27,6 +28,10 @@ public class Sources extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sources);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Select Sources");
 
         LayoutInflater inflater = this.getLayoutInflater();
         View finishButton = inflater.inflate(R.layout.activity_sources, null);
--- app/src/main/java/edu/illinois/g31/worldview/Topics.java
+++ app/src/main/java/edu/illinois/g31/worldview/Topics.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
+import android.support.v7.widget.Toolbar;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -82,6 +84,10 @@ public class Topics extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topics);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Select Topics");
 
         LayoutInflater inflater = this.getLayoutInflater();
         View nextButton = inflater.inflate(R.layout.activity_topics, null);
